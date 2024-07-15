b68b521b17ee88c4f91c494abfa982b674f15796
Heisdera <raphaelwisdom1@gmail.com>
2024-07-14 21:42:11 -0700

    Add mobile responsiveness

--- components/content.tsx
+++ components/content.tsx
@@ -9,37 +9,36 @@ const Content: NextPage<ContentType> = ({ className = "" }) => {
   return (
     <section className={[styles.content, className].join(" ")}>
       <div className={styles.highAngleYoungPeopleChurchParent}>
-        <img
+        {/* <img
           className={styles.highAngleYoungPeopleChurchIcon}
           alt=""
           src="/highangleyoungpeoplechurchpraying-1@2x.png"
           data-scroll-to="highAngleYoungPeopleChurchImage"
-        />
-        <h1 className={styles.secondaryHeadline}>
+        /> */}
+
+        <div className={styles.secondaryHeadline}>
           <p
             className={styles.over500Synergy}
           >{`Over 500 Synergy Community members cannot be wrong at the same time! `}</p>
           <p className={styles.joinTheSynergy}>
             Join the Synergy Community Today!
           </p>
-        </h1>
-      </div>
-      <div className={styles.paragraph}>
-        Don’t let life’s challenges hold you back. Become a part of the Synergy
-        Community and start your journey towards personal growth, financial
-        success, and a balanced lifestyle.
-      </div>
-      <div className={styles.synergize}>
-        <div className={styles.synergizeCallToAction}>
-          <div className={styles.readyToSynergizeWrapper}>
-            <b className={styles.readyToSynergizeContainer}>
-              <p className={styles.readyToSynergize}>Ready to Synergize?</p>
-            </b>
-          </div>
+        </div>
+
+        <div className={styles.paragraph}>
+          Don’t let life’s challenges hold you back. Become a part of the Synergy
+          Community and start your journey towards personal growth, financial
+          success, and a balanced lifestyle.
+        </div>
+
+        <div className={styles.synergize}>
+          <b className={styles.readyToSynergizeContainer}>
+            <p className={styles.readyToSynergize}>Ready to Synergize?</p>
+          </b>
+
           <button className={styles.signUp}>
-            <div className={styles.signUpButton}>
-              <div className={styles.signUpFor}>Sign Up for Free</div>
-            </div>
+            <span className={styles.signUpFor}>Learn More</span>
+
             <img
               className={styles.iconIconoirArrowRight}
               alt=""
@@ -48,7 +47,7 @@ const Content: NextPage<ContentType> = ({ className = "" }) => {
           </button>
         </div>
       </div>
-    </section>
+    </section >
   );
 };
 
--- components/frame-component.tsx
+++ components/frame-component.tsx
@@ -1,3 +1,4 @@
+
 import type { NextPage } from "next";
 import CategoryGrid from "./category-grid";
 import styles from "./frame-component.module.css";
@@ -9,107 +10,238 @@ export type FrameComponentType = {
 const FrameComponent: NextPage<FrameComponentType> = ({ className = "" }) => {
   return (
     <section
-      className={[styles.mainHeadlineParent, className].join(" ")}
+      className={[styles.challenges, className].join(" ")}
+      // className={styles.challenges}
       data-scroll-to="frameSection"
     >
-      <div className={styles.mainHeadline}>Headline One</div>
       <h1 className={styles.secondaryHeadline}>
         Are you facing these challenges?
       </h1>
-      <div className={styles.categoryGrid}>
-        <img
-          className={styles.directingArrowUpConceptIllIcon}
-          loading="lazy"
-          alt=""
-          src="/directing-arrow-up-concept-illustration.png"
-        />
-      </div>
-      <div className={styles.categoryGrid1}>
-        <div className={styles.titleCategory}>
-          <div className={styles.category}>Category</div>
-          <div className={styles.titleContainer}>
-            <div className={styles.title}>
-              Feeling Stuck in Your Personal Growth?
+
+      <div className={styles.categoryContentWrapper}>
+        <div className={styles.emptyIconWrapper}>
+          <img
+            className={styles.emptyIcon}
+            loading="lazy"
+            alt=""
+            src="/frame-1321317010@2x.png"
+          />
+        </div>
+
+        <div className={styles.categoryContent}>
+          {/* Create a Category component and passing the props needed just like you did category-grid */}
+          <div className={styles.category}>
+            <div className={styles.categoryGrid}>
+              <img
+                className={styles.directingArrowUpConceptIllIcon}
+                loading="lazy"
+                alt=""
+                src="/directing-arrow-up-concept-illustration.png"
+              />
             </div>
-            <div className={styles.badge1}>
-              <img className={styles.iconJamIconsOutlineL4} alt="" />
-              <div className={styles.textContainer2}>
-                <div className={styles.buttonText2}>+2,5%</div>
-              </div>
-              <img className={styles.iconIconoirStatUp} alt="" />
+
+            <div>
+              <h3 className={styles.title}>
+                Feeling Stuck in Your Personal Growth?
+              </h3>
+
+              <p className={styles.lifeIsBusy}>
+                Life is busy, and finding time for personal development can be
+                tough. You know you need to grow, learn, and improve, but where do
+                you start?
+              </p>
             </div>
-            <img
-              className={styles.iconJamIconsOutlineL5}
-              alt=""
-              src="/icon--jamicons--outline--logos--pencil.svg"
-            />
+          </div>
+
+          <div className={styles.category}>
+            <div className={styles.categoryGrid}>
+              <img
+                className={styles.directingArrowUpConceptIllIcon}
+                loading="lazy"
+                alt=""
+                src="/directing-arrow-up-concept-illustration.png"
+              />
+            </div>
+
+            <div>
+              <h3 className={styles.title}>
+                Feeling Stuck in Your Personal Growth?
+              </h3>
+
+              <p className={styles.lifeIsBusy}>
+                Life is busy, and finding time for personal development can be
+                tough. You know you need to grow, learn, and improve, but where do
+                you start?
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.category}>
+            <div className={styles.categoryGrid}>
+              <img
+                className={styles.directingArrowUpConceptIllIcon}
+                loading="lazy"
+                alt=""
+                src="/directing-arrow-up-concept-illustration.png"
+              />
+            </div>
+
+            <div>
+              <h3 className={styles.title}>
+                Feeling Stuck in Your Personal Growth?
+              </h3>
+
+              <p className={styles.lifeIsBusy}>
+                Life is busy, and finding time for personal development can be
+                tough. You know you need to grow, learn, and improve, but where do
+                you start?
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.category}>
+            <div className={styles.categoryGrid}>
+              <img
+                className={styles.directingArrowUpConceptIllIcon}
+                loading="lazy"
+                alt=""
+                src="/directing-arrow-up-concept-illustration.png"
+              />
+            </div>
+
+            <div>
+              <h3 className={styles.title}>
+                Feeling Stuck in Your Personal Growth?
+              </h3>
+
+              <p className={styles.lifeIsBusy}>
+                Life is busy, and finding time for personal development can be
+                tough. You know you need to grow, learn, and improve, but where do
+                you start?
+              </p>
+            </div>
           </div>
         </div>
-        <div className={styles.paragraph}>
-          <p className={styles.lifeIsBusy}>
-            Life is busy, and finding time for personal development can be
-            tough. You know you need to grow, learn, and improve, but where do
-            you start?
-          </p>
-        </div>
       </div>
-      <div className={styles.categoryGrid2}>
-        <img
-          className={styles.illustrationMeditation1Icon}
-          loading="lazy"
-          alt=""
-          src="/illustration-meditation.png"
-        />
-      </div>
-      <CategoryGrid
-        title="In Need of Wellness and Balance?"
-        iconJamIconsOutlineLogos="pending_I234:629;27:6393"
-        iconIconoirStatUp="pending_I234:629;27:6396"
-        paragraph="Balancing your mental, emotional, and physical health is crucial but challenging. it’s easy to feel lost in the daily grind without a structured wellness journey."
-        iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
-      />
-      <div className={styles.categoryGrid3}>
-        <img
-          className={styles.networkConceptIllustration1Icon}
-          loading="lazy"
-          alt=""
-          src="/network-concept-illustration.png"
-        />
-      </div>
-      <CategoryGrid
-        title="Lacking a Supportive Network?"
-        iconJamIconsOutlineLogos="/icon--jamicons--outline--logos--circle.svg"
-        iconIconoirStatUp="/icon--iconoir--statup-2.svg"
-        paragraph="Networking and making meaningful connections can be daunting. It's hard to find a group of like-minded individuals who share your goals and aspirations. But don’t worry! Associating with our community will help you build meaningful connections."
-        propTop="633.5px"
-        propLeft="102px"
-        iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
-      />
-      <div className={styles.categoryGrid4}>
-        <img
-          className={styles.financialCrisisBusinessFailIcon}
-          loading="lazy"
-          alt=""
-          src="/financialcrisisbusinessfailureflatconceptwithfallingpersonimagescoinsbanknotesarrowsdownvectorillustration-1@2x.png"
-        />
-      </div>
-      <CategoryGrid
-        title="Struggling to Improve Your Financial Situation?"
-        iconJamIconsOutlineLogos="/icon--jamicons--outline--logos--circle.svg"
-        iconIconoirStatUp="/icon--iconoir--statup-2.svg"
-        paragraph="Financial education often feels like a maze. Whether it's managing your income, learning to invest, or understanding the basics of finance, it can be overwhelming without the right guidance."
-        propTop="633.5px"
-        propLeft="524px"
-        iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
-      />
-      <img
-        className={styles.emptyIcon}
-        loading="lazy"
-        alt=""
-        src="/frame-1321317010@2x.png"
-      />
     </section>
   );
 };
 
 export default FrameComponent;
+
+
+// import type { NextPage } from "next";
+// import CategoryGrid from "./category-grid";
+// import styles from "./frame-component.module.css";
+
+// export type FrameComponentType = {
+//   className?: string;
+// };
+
+// const FrameComponent: NextPage<FrameComponentType> = ({ className = "" }) => {
+//   return (
+//     <section
+//       className={[styles.mainHeadlineParent, className].join(" ")}
+//       data-scroll-to="frameSection"
+//     >
+//       <div className={styles.mainHeadline}>Headline One</div>
+//       <h1 className={styles.secondaryHeadline}>
+//         Are you facing these challenges?
+//       </h1>
+//       <div className={styles.categoryGrid}>
+//         <img
+//           className={styles.directingArrowUpConceptIllIcon}
+//           loading="lazy"
+//           alt=""
+//           src="/directing-arrow-up-concept-illustration.png"
+//         />
+//       </div>
+//       <div className={styles.categoryGrid1}>
+//         <div className={styles.titleCategory}>
+//           <div className={styles.category}>Category</div>
+//           <div className={styles.titleContainer}>
+//             <div className={styles.title}>
+//               Feeling Stuck in Your Personal Growth?
+//             </div>
+//             <div className={styles.badge1}>
+//               <img className={styles.iconJamIconsOutlineL4} alt="" />
+//               <div className={styles.textContainer2}>
+//                 <div className={styles.buttonText2}>+2,5%</div>
+//               </div>
+//               <img className={styles.iconIconoirStatUp} alt="" />
+//             </div>
+//             <img
+//               className={styles.iconJamIconsOutlineL5}
+//               alt=""
+//               src="/icon--jamicons--outline--logos--pencil.svg"
+//             />
+//           </div>
+//         </div>
+//         <div className={styles.paragraph}>
+//           <p className={styles.lifeIsBusy}>
+//             Life is busy, and finding time for personal development can be
+//             tough. You know you need to grow, learn, and improve, but where do
+//             you start?
+//           </p>
+//         </div>
+//       </div>
+//       <div className={styles.categoryGrid2}>
+//         <img
+//           className={styles.illustrationMeditation1Icon}
+//           loading="lazy"
+//           alt=""
+//           src="/illustration-meditation.png"
+//         />
+//       </div>
+//       <CategoryGrid
+//         title="In Need of Wellness and Balance?"
+//         iconJamIconsOutlineLogos="pending_I234:629;27:6393"
+//         iconIconoirStatUp="pending_I234:629;27:6396"
+//         paragraph="Balancing your mental, emotional, and physical health is crucial but challenging. it’s easy to feel lost in the daily grind without a structured wellness journey."
+//         iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
+//       />
+//       <div className={styles.categoryGrid3}>
+//         <img
+//           className={styles.networkConceptIllustration1Icon}
+//           loading="lazy"
+//           alt=""
+//           src="/network-concept-illustration.png"
+//         />
+//       </div>
+//       <CategoryGrid
+//         title="Lacking a Supportive Network?"
+//         iconJamIconsOutlineLogos="/icon--jamicons--outline--logos--circle.svg"
+//         iconIconoirStatUp="/icon--iconoir--statup-2.svg"
+//         paragraph="Networking and making meaningful connections can be daunting. It's hard to find a group of like-minded individuals who share your goals and aspirations. But don’t worry! Associating with our community will help you build meaningful connections."
+//         propTop="633.5px"
+//         propLeft="102px"
+//         iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
+//       />
+//       <div className={styles.categoryGrid4}>
+//         <img
+//           className={styles.financialCrisisBusinessFailIcon}
+//           loading="lazy"
+//           alt=""
+//           src="/financialcrisisbusinessfailureflatconceptwithfallingpersonimagescoinsbanknotesarrowsdownvectorillustration-1@2x.png"
+//         />
+//       </div>
+//       <CategoryGrid
+//         title="Struggling to Improve Your Financial Situation?"
+//         iconJamIconsOutlineLogos="/icon--jamicons--outline--logos--circle.svg"
+//         iconIconoirStatUp="/icon--iconoir--statup-2.svg"
+//         paragraph="Financial education often feels like a maze. Whether it's managing your income, learning to invest, or understanding the basics of finance, it can be overwhelming without the right guidance."
+//         propTop="633.5px"
+//         propLeft="524px"
+//         iconJamIconsOutlineLogos1="/icon--jamicons--outline--logos--pencil.svg"
+//       />
+//       <img
+//         className={styles.emptyIcon}
+//         loading="lazy"
+//         alt=""
+//         src="/frame-1321317010@2x.png"
+//       />
+//     </section>
+//   );
+// };
+
+// export default FrameComponent;
